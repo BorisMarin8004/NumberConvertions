19d597588b895ee44f8a2492ab1a2d65345c0301
BorisMarin <bmar8004@gmail.com>
2023-01-29 19:20:07 -0800

    Added dot handling

--- main.py
+++ main.py
@@ -44,31 +44,49 @@ def n_to_s(target):
 
 def to_dec(init_base: int, num: str) -> int:
     res = 0
-    rev_num = num[::-1]
-    for power, digit in enumerate(rev_num):
+    num_whole, num_partial = num.split(".")
+    rev_num_whole = num_whole[::-1]
+    for power, digit in enumerate(rev_num_whole):
         if show_operations:
             print(f"{res} + {s_to_n(digit)} * {init_base}^{power}")
         res += s_to_n(digit) * pow(init_base, power)
+    for power, digit in enumerate(num_partial):
+        if show_operations:
+            print(f"{res} + {s_to_n(digit)} * {init_base}^{-(power+1)}")
+        res += s_to_n(digit) * pow(init_base, -(power+1))
     return res
 
 
-def from_dec(dest_base: int, num: int) -> str:
+def from_dec(dest_base: int, num: float) -> str:
     dest_num = ""
+    num_whole = int(num)
+    num_partial = num % 1
     while True:
         if show_operations:
-            print(f"{num}/{dest_base} - > {num % dest_base}")
-        dest_num += n_to_s(num % dest_base)
-        num = int(num / dest_base)
-        if num == 0:
+            print(f"{num_whole}/{dest_base} - > {num_whole % dest_base}")
+        dest_num += n_to_s(num_whole % dest_base)
+        num_whole = int(num_whole / dest_base)
+        if num_whole == 0:
             break
-    return dest_num[::-1]
+    dest_num = dest_num[::-1]
+    if num_partial % 1 != 0:
+        dest_num += "."
+        while True:
+            if show_operations:
+                print(f"{num_partial} * {dest_base} - > {num_partial*dest_base}")
+            num_partial *= dest_base
+            dest_num += str(int(num_partial))
+            num_partial %= 1
+            if num_partial % 1 == 0:
+                break
+    return dest_num
 
 
 def convert(to_base: int, from_base: int, num_to_convert: str) -> str:
     if from_base == to_base:
         return num_to_convert
     if from_base == 10:
-        return from_dec(to_base, int(num_to_convert))
+        return from_dec(to_base, float(num_to_convert))
     if to_base == 10:
         return str(to_dec(from_base, num_to_convert))
     return from_dec(to_base, to_dec(from_base, num_to_convert))
@@ -97,7 +115,7 @@ def operation_with_any_numbers() -> str:
     if res < 0:
         print(f"\n***** Result was negative the number needs to be negated *****\n    Number base 10: {res} \n")
         res = convert(int(num1_base), 10, str(abs(res)))
-        return "(1)"+negate(int(num1_base), res)
+        return "(1)" + negate(int(num1_base), res)
     return convert(int(num1_base), 10, str(res))
 
 
